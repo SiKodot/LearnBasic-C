ae96dc233e1197237f3e6905b123fcb764534c63
SiKodot <ihsangamersfauzi123@gmail.com>
2024-12-25 23:48:51 +0700

    v1

---
+++ 1.00-intro.c
@@ -0,0 +1,15 @@
+/* 
+Deskripsi : menampilkan bilangan dua kali lipat
+Nama /Author : Ihsan Fauzi
+Tanggal/versi : 21 Oktober 2024
+*/
+
+#include<stdio.h>
+
+int main(){
+    int x,y,result;
+    scanf("%d %d", &x, &y);
+    result=x+y;
+    printf("%d", result);
+    return 0;
+}
