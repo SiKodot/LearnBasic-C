d2a486ee83f2725f20559201cc0bdaec587a77ce
SiKodot <ihsangamersfauzi123@gmail.com>
2025-01-14 23:08:50 +0700

    v16(bonus2)

---
+++ bonus2.02-LuasNLingkaran.c
@@ -0,0 +1,14 @@
+/*
+Nama    : Ihsan Fauzi
+Kelas   : 1B-D4
+*/
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    float r,luas;
+    scanf("%f %d", &r, &n);
+    luas=(float) r*r*n* 3.14;
+    printf("%.2f", luas);
+}
