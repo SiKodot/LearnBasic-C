aade8f64d61aea9a7daee87ecaa9034e6267b768
SiKodot <ihsangamersfauzi123@gmail.com>
2025-01-15 22:16:00 +0700

    v17(bonus 3)

---
+++ bonus2.03-VolumeTabung.c
@@ -0,0 +1,14 @@
+/*
+Nama    : Ihsan Fauzi
+Kelas   : 1B-D4
+*/
+
+#include<stdio.h>
+
+int main(){
+    int r,t;
+    float hasil;
+    scanf("%d %d", &r, &t);
+    hasil=(float) r*r*t* 3.14;
+    printf("%.2f", hasil);
+}
