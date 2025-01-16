07663bbb8a298aa960a3cb7497543c50dde946ee
SiKodot <ihsangamersfauzi123@gmail.com>
2025-01-16 22:48:06 +0700

    v18(bonus 4)

---
+++ bonus2.04-Jari-jariLingkaran.c
@@ -0,0 +1,15 @@
+/*
+Nama    : Ihsan Fauzi
+Kelas   : 1B-D4
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    float hasil,l;
+    scanf("%f", &hasil);
+    l=(float) hasil/3.14;
+    l=sqrt(l);
+    printf("%.2f", l);
+}
